cc68e44cf0956ba67d530ebd220fd7edb38c3fbf
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2022-10-10 21:43:30 +0800

    Wrap App with ChakraUI & Auth0 Provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { ChakraProvider } from "@chakra-ui/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-vu05pq1i.au.auth0.com"
+      clientId="Aiwl0uJMs99Nkm642Xz9rVDNeKLtH69J"
+      redirectUri={window.location.origin}
+    >
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
